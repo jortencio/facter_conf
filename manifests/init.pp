5a85d641063575ae0f763cca474658d968616803
Jason Ortencio <jason.ortencio@puppet.com>
2023-08-24 17:36:02 +1000

    Add config path parameter and data

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,14 @@
-# @summary A short summary of the purpose of this class
+# @summary A class for configuring facter.conf
 #
-# A description of what this class does
+# Configures facter.conf based on settings available at https://www.puppet.com/docs/puppet/latest/configuring_facter.html
+#
+# @param config_path
+#
+#  Path for configuring facter.conf
 #
 # @example
 #   include facter_conf
-class facter_conf {
+class facter_conf (
+  String $config_path,
+) {
 }
