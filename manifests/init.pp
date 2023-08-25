656c34e3132ea170e09321419940130570a5bc57
Jason Ortencio <jason.ortencio@puppet.com>
2023-08-25 10:58:17 +1000

    Add facter.conf parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,33 @@
 # @example
 #   include facter_conf
 class facter_conf (
-  String $config_path,
+  Boolean                    $manage_facter_conf = true,
+  String                     $config_path = '/etc/puppetlabs/facter/facter.conf',
+  Optional[Array[String[1]]] $facts_blocklist = undef,
+  Optional[Array[Hash]]      $facts_ttls = undef,
+  Optional[Array[String[1]]] $global_external_dir = undef,
+  Optional[Array[String[1]]] $global_custom_dir = undef,
+  Optional[Boolean]          $global_no_external = undef,
+  Optional[Boolean]          $global_no_custom = undef,
+  Optional[Boolean]          $global_no_ruby = undef,
+  Optional[Boolean]          $cli_debug = undef,
+  Optional[Boolean]          $cli_trace = undef,
+  Optional[Boolean]          $cli_verbose = undef,
+  Optional[Enum[
+      'none',
+      'fatal',
+      'error',
+      'warn',
+      'info',
+      'debug',
+      'trace'
+  ]]                         $cli_log_level = undef,
+  Optional[Hash]             $fact_groups = undef,
 ) {
+  if $manage_facter_conf {
+    file { "${config_path}/facter.conf":
+      ensure  => file,
+      content => epp('facter_conf/facter.conf.epp'),
+    }
+  }
 }
