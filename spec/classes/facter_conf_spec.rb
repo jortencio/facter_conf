0a91186b4153de253332c84e832b71512f403986
Jason Ortencio <jason.ortencio@puppet.com>
2023-08-29 10:07:38 +1000

    Add unit test

--- spec/classes/facter_conf_spec.rb
+++ spec/classes/facter_conf_spec.rb
@@ -7,6 +7,15 @@ describe 'facter_conf' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
+      if os_facts[:os]['family'] == 'windows'
+        facter_path = 'C:/ProgramData/PuppetLabs/facter/etc'
+      else
+        facter_path = '/etc/puppetlabs/facter'
+      end
+
+      it { is_expected.to contain_file(facter_path) }
+      it { is_expected.to contain_file("#{facter_path}/facter.conf") }
+
       it { is_expected.to compile.with_all_deps }
     end
   end
