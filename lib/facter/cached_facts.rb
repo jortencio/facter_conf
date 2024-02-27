48ddd8139cad64a9d81a91dce715f6ee55f2ab54
Jason Ortencio <jason.ortencio@puppet.com>
2024-02-27 18:03:49 +1100

    add cached_facts fact

---
+++ lib/facter/cached_facts.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'hocon'
+
+Facter.add(:cached_facts) do
+  confine facter_conf_exists: true
+  setcode do
+    facter_conf_path = if Facter.value('os')['family'] == 'windows'
+                         'C:/ProgramData/PuppetLabs/facter/etc/facter.conf'
+                       else
+                         '/etc/puppetlabs/facter/facter.conf'
+                       end
+    facter_conf = Hocon.load(facter_conf_path)
+    facter_conf
+  end
+end
